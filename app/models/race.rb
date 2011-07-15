b253b915a17a31a5e885593b4ffb1ed3edf40f8c
Anthony Metcalf <nevyn@anferny.me.uk>
2011-07-15 16:06:22 +0100

    More rigerous testing of races

--- app/models/race.rb
+++ app/models/race.rb
@@ -2,6 +2,12 @@ class Race < ActiveRecord::Base
 	has_many :characters
 
 	validates :name, :presence => true
+	validates :str_mod, :numericality => { :only_integer => true }
+	validates :dex_mod, :numericality => { :only_integer => true }
+	validates :con_mod, :numericality => { :only_integer => true }
+	validates :int_mod, :numericality => { :only_integer => true }
+	validates :wis_mod, :numericality => { :only_integer => true }
+	validates :cha_mod, :numericality => { :only_integer => true }
 end
 
 # == Schema Information
--- spec/models/races_spec.rb
+++ spec/models/races_spec.rb
@@ -1,14 +1,58 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Race do
-  it "should be invalid without a name" do
-    @race = Race.new
-    @race.should_not be_valid
-    @race.errors.on(:name).should == "can't be blank"
+	before(:each) do
+		@race = Race.new
 	end
 
-	it "should be valid with a name" do
-    @race = Race.new
+  it "should be invalid without a name" do
+    @race.should_not be_valid
+    @race.errors[:name].should == ["can't be blank"]
+	end
+
+	it "should be invalid with non-integer str_mod" do
+    @race.name = 'someracename'
+		@race.str_mod = "hello"
+    @race.should_not be_valid
+    @race.errors[:str_mod].should == ["is not a number"]
+	end
+
+	it "should be invalid with non-integer dex_mod" do
+    @race.name = 'someracename'
+		@race.dex_mod = "hello"
+    @race.should_not be_valid
+    @race.errors[:dex_mod].should == ["is not a number"]
+	end
+
+	it "should be invalid with non-integer con_mod" do
+    @race.name = 'someracename'
+		@race.con_mod = "hello"
+    @race.should_not be_valid
+    @race.errors[:con_mod].should == ["is not a number"]
+	end
+
+	it "should be invalid with non-integer int_mod" do
+    @race.name = 'someracename'
+		@race.int_mod = "hello"
+    @race.should_not be_valid
+    @race.errors[:int_mod].should == ["is not a number"]
+	end
+
+	it "should be invalid with non-integer wis_mod" do
+    @race.name = 'someracename'
+		@race.wis_mod = "hello"
+    @race.should_not be_valid
+    @race.errors[:wis_mod].should == ["is not a number"]
+	end
+
+	it "should be invalid with non-integer cha_mod" do
+    @race.name = 'someracename'
+		@race.cha_mod = "hello"
+    @race.should_not be_valid
+    @race.errors[:cha_mod].should == ["is not a number"]
+	end
+
+	it "should be valid with valid details" do
     @race.name = 'someracename'
     @race.should be_valid
   end
