f294abb0791696b4975148eb103377a9107bc2d5
Anthony Metcalf <nevyn@anferny.me.uk>
2011-07-15 15:33:29 +0100

    Added basic validation to race.rb. Needs extending

--- spec/models/races_spec.rb
+++ spec/models/races_spec.rb
@@ -4,10 +4,11 @@ describe Race do
   it "should be invalid without a name" do
     @race = Race.new
     @race.should_not be_valid
-    @race.errors.on(:name).should equal "is required"
+    @race.errors.on(:name).should == "can't be blank"
 	end
 
 	it "should be valid with a name" do
+    @race = Race.new
     @race.name = 'someracename'
     @race.should be_valid
   end
